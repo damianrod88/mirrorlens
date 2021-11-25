64ddff0405483ceab0a205cb93bb39cc62f53c17
Damian Rodriguez <damianrod@gmail.com>
2021-11-25 18:38:05 -0300

    register works

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,8 +1,11 @@
+const { json } = require("express");
 const fs = require("fs");
 const path = require("path");
 
 const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
 const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+const productsFileUser = path.join(__dirname, "../data/users.json");
+const users = JSON.parse(fs.readFileSync(productsFileUser, "utf-8"));
 
 const mainController = {
     home: function (req, res) {
@@ -63,6 +66,69 @@ const mainController = {
         res.render("formularioEditor", {
             pageTitle: "Modifica tu producto",
         });
+    },
+    store: function (req, res) {
+        let producto = {
+            nombre: req.body.nombre,
+            img: req.file.filename,
+            model: req.body.model,
+            color: req.body.color,
+            price: req.body.price,
+            descr: req.body.descr,
+            moneda: req.body.moneda,
+        };
+
+        const pro = fs.readFileSync(
+            path.join(__dirname, "../data/producto.json"),
+            "utf-8"
+        );
+        let prod;
+        if (pro == "") {
+            prod = [];
+        } else {
+            prod = JSON.parse(pro);
+        }
+        prod.push(producto);
+
+        productoJSON = JSON.stringify(prod);
+        fs.writeFileSync(
+            path.join(__dirname, "../data/producto.json"),
+            usuarioJSON
+        );
+        if (req.file) {
+            producto.img = req.file.filename;
+            productoId = mainController.create(producto);
+            res.redirect("/collections/" + productoId);
+        }
+    },
+    cRegister: function (req, res) {
+        let usuario = {
+            nombre: req.body.nombre,
+            user: req.body.user,
+            mail: req.body.mail,
+            birdate: req.body.birdate,
+            direction: req.body.direction,
+            pass: req.body.pass,
+            repass: req.body.repass,
+        };
+        const userss = fs.readFileSync(
+            path.join(__dirname, "../data/userss.json"),
+            "utf-8"
+        );
+        let usuarios;
+        if (userss == "") {
+            usuarios = [];
+        } else {
+            usuarios = JSON.parse(userss);
+        }
+        usuarios.push(usuario);
+        //users.push(usuario);
+        usuarioJSON = JSON.stringify(usuarios);
+        fs.writeFileSync(
+            path.join(__dirname, "../data/userss.json"),
+            usuarioJSON
+        );
+        res.render("thanksForR");
     },
 };
 
--- src/routers/main.js
+++ src/routers/main.js
@@ -1,16 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 const mainController = require("../controllers/mainController");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "../../public/images/productsImage"));
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        const newFileName =
+            "user-" + Date.now() + path.extname(file.originalname);
+        cb(null, newFileName);
+    },
+});
+
+const upload = multer({ storage: storage });
 
 router.get("/", mainController.home);
 router.get("/cart", mainController.cart);
 router.get("/login", mainController.login);
 router.get("/register", mainController.register);
+router.post("/register", mainController.cRegister);
 
 router.get("/collections", mainController.shop);
 router.get("/collections/:id", mainController.detalle);
 router.get("/search", mainController.search);
 router.get("/create", mainController.create);
+router.post("/create", upload.single("image"), mainController.store);
 router.get("/modify", mainController.modify);
 
 module.exports = router;
