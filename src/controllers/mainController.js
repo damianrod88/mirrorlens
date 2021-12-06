c5968ae3de7a378cecdfb1bcbdbf9eb51d8cd954
Damian Rodriguez <damianrod@gmail.com>
2021-12-05 21:20:39 -0300

    add errors to register

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,6 +5,7 @@ const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
 const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 const usersFilePath = path.join(__dirname, "../data/users.json");
 const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+const { validationResult } = require("express-validator");
 
 const controller = {
     home: function (req, res) {
@@ -41,16 +42,24 @@ const controller = {
     },
 
     cRegister: function (req, res) {
-        let usuario = {
-            id: Date.now(),
-            ...req.body,
-        };
+        const resultValidation = validationResult(req);
+        if (resultValidation.errors.length > 0) {
+            return res.render("register", {
+                errors: resultValidation.mapped(),
+                oldData: req.body,
+            });
+        } else {
+            let usuario = {
+                id: Date.now(),
+                ...req.body,
+            };
 
-        users.push(usuario);
+            users.push(usuario);
 
-        usuarioJSON = JSON.stringify(users, null, 2);
-        fs.writeFileSync(usersFilePath, usuarioJSON);
-        res.render("thanksForR");
+            usuarioJSON = JSON.stringify(users, null, 2);
+            fs.writeFileSync(usersFilePath, usuarioJSON);
+            res.render("thanksForR");
+        }
     },
 };
 
--- src/routers/main.js
+++ src/routers/main.js
@@ -3,6 +3,41 @@ const router = express.Router();
 const multer = require("multer");
 const path = require("path");
 const mainController = require("../controllers/mainController");
+const { body } = require("express-validator");
+
+const validator = [
+    body("name")
+        .notEmpty()
+        .withMessage("Tienes que escribir tu nombre y apellido"),
+    body("user").notEmpty().withMessage("Tienes que escribir un usuario"),
+    body("mail")
+        .notEmpty()
+        .withMessage("Tienes que escribir tu mail")
+        .bail()
+        .isEmail()
+        .withMessage("Debes escribir un formato de correo válido"),
+    body("direction")
+        .notEmpty()
+        .withMessage("Tienes que escribir tu dirección"),
+    body("pass").notEmpty().withMessage("Tienes que escribir una contrañesa"),
+    body("repass").notEmpty().withMessage("Tienes que escribir una contraseña"),
+    /* body("img").custom((value, { req }) => {
+        let file = req.file;
+        let acceptedExt = [".jpg", ".png"];
+        if (!file) {
+            throw new Error("Tienes que subir una imagen");
+        } else {
+            let fileExt = path.extname(file.originalname);
+            if (!acceptedExt.includes(fileExt)) {
+                throw new Error(
+                    `Las extensiones permitidas son ${acceptedExt.join(",")}`
+                );
+            }
+        }
+
+        return true;
+    }),*/
+];
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -22,7 +57,7 @@ router.get("/", mainController.home);
 router.get("/cart", mainController.cart);
 router.get("/login", mainController.login);
 router.get("/register", mainController.register);
-router.post("/register", mainController.cRegister);
+router.post("/register", validator, mainController.cRegister);
 router.get("/search", mainController.search);
 
 module.exports = router;
